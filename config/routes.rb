75e1c6ff592b4ee7bca138b50a78b000708dd3de
hoasung01 <nguyenngochai.shipagent@gmail.com>
2017-02-17 01:47:09 +0700

    upload upload form with condition is csv file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'visitors#index'
+  resources :gg_search, only: [] do
+    post 'upload_csv', on: :collection
+  end
   namespace :api do
     namespace :v1 do
     end
