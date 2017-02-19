743713aeffb1384c7dddd2e813c4316be618a08b
hoasung01 <nguyenngochai.shipagent@gmail.com>
2017-02-19 22:54:53 +0700

    update field in gg_result_page can store with array type

--- app/models/gg_result_page.rb
+++ app/models/gg_result_page.rb
@@ -1,3 +1,6 @@
 class GgResultPage < ActiveRecord::Base
+  serialize :url_non_adword, Array
+  serialize :green_url_adword_top, Array
+  serialize :green_url_adword_bottom, Array
   belongs_to :gg_keyword
 end
