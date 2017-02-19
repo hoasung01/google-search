72369b9a2cb57cc7b830b872a7e495174f805c12
hoasung01 <nguyenngochai.shipagent@gmail.com>
2017-02-19 11:11:53 +0700

    add create_keyword_from_csv in gg_keyword model

--- app/models/gg_keyword.rb
+++ app/models/gg_keyword.rb
@@ -1,3 +1,15 @@
+require 'csv'
+
 class GgKeyword < ActiveRecord::Base
   has_one :gg_result_page, dependent: :destroy
+
+  def create_keyword_from_csv(file_name)
+    file_path = Rails.root.join "public", "#{file_name}"
+
+    CSV.foreach(file_path) do |row|
+      row.each do |keyword|
+        GgKeyword.create(name: keyword)
+      end
+    end    
+  end
 end
