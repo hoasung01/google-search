90709684b15bb029510c0a8b99e6b3ae332d6d7c
hoasung01 <nguyenngochai.shipagent@gmail.com>
2017-02-21 00:46:19 +0700

    show google keyword name in report

--- app/models/gg_result_page.rb
+++ app/models/gg_result_page.rb
@@ -3,4 +3,5 @@ class GgResultPage < ActiveRecord::Base
   serialize :green_url_adword_top, Array
   serialize :green_url_adword_bottom, Array
   belongs_to :gg_keyword
+  delegate :name, to: :gg_keyword, allow_nil: true, prefix: true
 end
